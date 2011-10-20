28fb77cbe504d7f2239e6724fdaf8a6d2a2b8169
Stian Mathiassen <stian.mathiassen@bekk.no>
2011-10-20 15:34:17 +0200

    Links in faggruppe

--- src/main/java/no/bekk/java/jersey/exercise/dto/FaggruppeDto.java
+++ src/main/java/no/bekk/java/jersey/exercise/dto/FaggruppeDto.java
@@ -1,20 +1,26 @@
 package no.bekk.java.jersey.exercise.dto;
 
+import com.sun.jersey.server.linking.Ref;
 import no.bekk.java.jersey.exercise.model.Faggruppe;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
+import java.net.URI;
 
 @XmlRootElement(name = "Faggruppe")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class FaggruppeDto {
-
-    @XmlTransient
     public Long id;
     public Long faggruppesjefId;
     public String navn;
+
+    @Ref(value = "faggruppe/{id}", style = Ref.Style.ABSOLUTE)
+	public URI link;
+
+    @Ref(value = "ansatt/{faggruppesjefId}", style = Ref.Style.ABSOLUTE)
+	public URI faggruppesjef;
 
     public FaggruppeDto() {
     }
@@ -23,5 +29,13 @@ public class FaggruppeDto {
         id = faggruppe.getId();
         navn = faggruppe.getNavn();
         faggruppesjefId = faggruppe.getFagruppesjefId();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getFaggruppesjefId() {
+        return faggruppesjefId;
     }
 }
